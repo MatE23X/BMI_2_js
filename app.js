00a4c5e64084d9df371fa0ec4ebe3a1a3f252f28
MaxE23X <matusalaalemseged411@gmail.com>
2023-10-16 10:07:50 -0400

    complete challenge

---
+++ app.js
@@ -0,0 +1,34 @@
+
+const mark = {
+    fullName : "Mark Miller",
+    mass : 78,
+    height : 1.69,
+
+    calcBMI : function(mass,height){
+        heightDeno = this.height * this.height;
+        BMI = this.mass / heightDeno;
+        return BMI;
+    }
+}
+const john = {
+    fullName : "John Smith",
+    mass : 92,
+    height: 1.95,
+
+    calcBMI : function(){
+        heightDeno = this.height * this.height;
+        BMI = this.mass / heightDeno;
+        return BMI;
+    }
+}
+//assign BMI to a property
+mark.BMI = mark.calcBMI();
+john.BMI = john.calcBMI();
+
+if (mark.BMI > john.BMI){
+    console.log(`Mark Miller's BMI(${mark.BMI})
+    is higher than John Smith's BMI(${john.BMI}).`);
+}
+else{
+    console.log(`John Smith's BMI (${john.BMI}) is higher than Mark Miller's(${mark.BMI}).`);
+}
